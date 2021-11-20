180aa3d4d5a720b12015ed2c25ebde77d1311b09
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-20 09:52:38 -0800

    feat: animate navbar and sidebars

--- gsap-tuts/src/components/Main/index.tsx
+++ gsap-tuts/src/components/Main/index.tsx
@@ -1,6 +1,23 @@
 import "./main.css";
+import { useEffect } from "react";
+import { gsap } from "gsap";
 
 export const Main = () => {
+  useEffect(() => {
+    gsap.from(".right", {
+      duration: 1,
+      x: "-100vw",
+      delay: 1,
+      ease: "power2.in",
+    });
+
+    gsap.from(".left", {
+      duration: 1,
+      x: "-100%",
+      delay: 1.5,
+      ease: "power2.in",
+    });
+  }, []);
   return (
     <div id="Main">
       <div className="content">
--- gsap-tuts/src/components/Navbar/index.tsx
+++ gsap-tuts/src/components/Navbar/index.tsx
@@ -1,11 +1,30 @@
 import "./navbar.css";
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 
 export const Navbar = () => {
+  const NavbarRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    gsap.from(NavbarRef.current, {
+      duration: 1,
+      y: "-100%",
+      ease: "power1",
+    });
+
+    gsap.from(".links", {
+      duration: 2,
+      opacity: 0,
+      delay: 1,
+      stagger: 0.5,
+    });
+  }, [NavbarRef]);
+
   return (
-    <div id="Navbar">
-      <h1>Link 1</h1>
-      <h1>Link 2</h1>
-      <h1>Link 3</h1>
+    <div ref={NavbarRef} id="Navbar">
+      <h1 className="links">Link 1</h1>
+      <h1 className="links">Link 2</h1>
+      <h1 className="links">Link 3</h1>
     </div>
   );
 };
