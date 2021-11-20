5cf5250543b0dee44e20d1b5fa6f8bb54b526787
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-20 10:51:43 -0800

    chore: add additional styles to button

--- gsap-tuts/src/components/Main/index.tsx
+++ gsap-tuts/src/components/Main/index.tsx
@@ -2,39 +2,42 @@ import "./main.css";
 import { useEffect } from "react";
 import { gsap } from "gsap";
 
-export const Main = () => {
+export const mainAnimate = () => {
   const timeline = gsap.timeline({ defaults: { duration: 1 } });
-
-  useEffect(() => {
-    timeline
-      .from(".right", {
-        x: "-100vw",
-        delay: 1,
+  timeline
+    .from(".right", {
+      x: "-100vw",
+      delay: 1,
+      ease: "power2.in",
+    })
+    .from(
+      ".left",
+      {
+        x: "-100%",
         ease: "power2.in",
-      })
-      .from(
-        ".left",
-        {
-          x: "-100%",
-          ease: "power2.in",
-        },
-        1
-      )
-      .fromTo(
-        ".button",
-        {
-          opacity: 0,
-          scale: 0,
-          rotation: 720,
-        },
-        {
-          opacity: 1,
-          scale: 1,
-          rotation: 0,
-        },
-        3
-      );
-  }, [timeline]);
+      },
+      1
+    )
+    .fromTo(
+      ".button",
+      {
+        opacity: 0,
+        scale: 0,
+        rotation: 720,
+      },
+      {
+        opacity: 1,
+        scale: 1,
+        rotation: 0,
+      },
+      3
+    );
+};
+
+export const Main = () => {
+  useEffect(() => {
+    mainAnimate();
+  }, []);
 
   return (
     <div id="Main">
