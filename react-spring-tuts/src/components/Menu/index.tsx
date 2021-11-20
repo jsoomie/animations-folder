5766dc1d2325f21712bfb4619e849c364b15784b
Jonathan Soukaseume <sksmejn@gmail.com>
2021-11-20 13:30:08 -0800

    chore: add quick fade in animation on menu

--- react-spring-tuts/src/components/Menu/index.tsx
+++ react-spring-tuts/src/components/Menu/index.tsx
@@ -1,9 +1,16 @@
+import { useSpring, animated } from "react-spring";
+
 export const Menu = () => {
+  const fade = useSpring({
+    from: { opacity: 0 },
+    opacity: 1,
+  });
+
   return (
-    <div>
+    <animated.div style={fade}>
       <h1>
         <a href="/">Menu</a>
       </h1>
-    </div>
+    </animated.div>
   );
 };
